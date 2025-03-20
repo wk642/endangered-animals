78c7aed0f588a40ebcdea2396f2a53533729329b
Winnie Kelley <w642kelley@gmail.com>
2025-03-20 06:27:44 -0500

    Database and table creation works, inserted to species table

--- database/db.sql
+++ database/db.sql
@@ -1,10 +1,17 @@
 -- create database
-CREATE DATABASE endangeredAnimalsSightingsDB;
+-- This drop is a little bit of an error handling, incase there was already this database we are going to drop it and create one. 
+DROP DATABASE "sightingsDB";
+CREATE DATABASE "sightingsDB";
+
+-- connecting to the database (if this is skipped, I did not know we can create tables outside of the database, but it sure did)
+\c "sightingsDB";
 
 -- creating the tables
 -- species table
 CREATE TABLE IF NOT EXISTS species (
-  id SERIAL PRIMARY KEY,
+  -- After speaking with Kaylah about security, decided to randomly generate the id instead using UUID to generate a random id instead (https://starkandwayne.com/blog/uuid-primary-keys-in-postgresql/)
+  -- id SERIAL PRIMARY KEY,
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   common_name VARCHAR(255),
   scientific_name VARCHAR(255),
   species_population INTEGER,
@@ -15,17 +22,36 @@ CREATE TABLE IF NOT EXISTS species (
 
 -- individuals table
 CREATE TABLE IF NOT EXISTS individuals (
-  id SERIAL PRIMARY KEY,
+  -- id SERIAL PRIMARY KEY NOT NULL,
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   individual_nickname VARCHAR (255),
-  speices VARCHAR (255),
+  -- this would be a foreign key 
+  species_id UUID REFERENCES species(id),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- sightings table
 CREATE TABLE IF NOT EXISTS sightings (
-  id SERIAL PRIMARY KEY,
+  -- id SERIAL PRIMARY KEY,
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   sighting_date_time TIMESTAMP,
+  sighted_animal_id UUID REFERENCES individuals(id),
   animal_health BOOLEAN,
   sighter_email VARCHAR(255),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+-- info from https://www.iucnredlist.org/resources/summary-statistics
+-- inserts into tables
+-- species - at least 3 
+INSERT INTO species (common_name, scientific_name, species_population, conservation_code) VALUES
+('Mammals', 'Felis catus', 6736, 'CNC'),
+('Birds', 'Turdus migratorius', 11195, 'CNC'),
+('Amphibians', 'Lithobates catesbeianus', 8776, 'CNC'),
+-- ('Fish', 'Oncorhynchus mykiss', 36953, 'CNC'),
+-- ('Insects', 'Apis mellifera', 1053578, 'CNC'),
+-- ('Arachnids', 'Araneus diadematus', 95966, 'CNC'),
+-- ('Crustaceans', 'Homarus americanus', 90820, 'CNC');
+
+-- individuals
+-- sightings
