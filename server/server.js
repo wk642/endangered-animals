5f31a4da1ec8e7756d8d3f04e58516bd156cbb87
Winnie Kelley <w642kelley@gmail.com>
2025-03-20 16:02:51 -0500

    Route to get all species

--- server/server.js
+++ server/server.js
@@ -1,14 +1,38 @@
 import express from 'express';
 import cors from 'cors';
+import pgPromise from 'pg-promise';
+// using npm documentation for pg-promise https://www.npmjs.com/package/pg-promise
+
 const app = express();
 const port = 5000; 
+
+// initialize pg-promise
+const pgp = pgPromise();
+// connect to the database
+const db = pgp('postgres://tpl622_6@localhost:5432/sightingsDB');
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+// testing to make sure this connects
+app.get('/test connections', (req, res) => {
   res.json("Welcome! Let's record some endangered animals");
 });
+
+// display all the species
+app.get('/species', async (req,res) => {
+  try {
+    const species = await db.any('SELECT * FROM species');
+    res.json(species);
+  } catch (error) {
+    console.error("Error in getting all the species");
+    res.status(500).json({error:'Error in getting all the species'})
+  }
+})
+
+// display all the individuals
+
+// display all the sightings
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
