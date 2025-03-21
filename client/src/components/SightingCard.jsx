d146c162ace675099bddb62690b8ed54d8f850ae
Winnie Kelley <w642kelley@gmail.com>
2025-03-21 09:10:13 -0500

    Added delete buttons to the front end

--- client/src/components/SightingCard.jsx
+++ client/src/components/SightingCard.jsx
@@ -6,7 +6,18 @@ function SightingCard({ sightings }) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {sightings.map((sighting) => (
-        <div key={sighting.id} className="bg-green-300 rounded-lg p-4">
+        <div key={sighting.id} className="relative bg-green-300 rounded-lg p-4">
+           {/* Add delete button for every card */}
+           <button
+            onClick={() => {
+              if (onDelete) {
+                onDelete(individual.id);
+              }
+            }}
+            className="absolute top-2 right-2 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded text-xs"
+          >
+            Delete
+          </button>
           <p><span className="font-bold">Date/Time:</span> {sighting.sighting_date_time}</p>
           <p><span className="font-bold">Animal:</span> {sighting.individual_nickname}</p>
           <p><span className="font-bold">Sighted Location:</span> {sighting.sighted_location}</p>
